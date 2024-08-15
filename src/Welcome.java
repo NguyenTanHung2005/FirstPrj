af19ac2f120ec2d3680844bd4096a7822113f91a
nguyentanhung2005 <23709281.hung@student.iuh.edu.vn>
2024-08-15 20:54:33 +0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Tan Hung!");
+        System.out.println("Welcome to the DHKTLT19B!");
+    }
+}
